a2138e920f9dd6565177b341a67ac4aecb3fee0c
Ziyang <Kimizx3@gmail.com>
2025-01-17 19:13:26 -0800

    moved

--- Assign1/cmake-build-debug/cmake_install.cmake
+++ Assign1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ziyangk/Desktop/Code/GameProgramming4300
+# Install script for directory: /Users/ziyangk/Desktop/Code/GameProgramming4300/Assign1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ziyangk/Desktop/Code/GameProgramming4300/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ziyangk/Desktop/Code/GameProgramming4300/Assign1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
